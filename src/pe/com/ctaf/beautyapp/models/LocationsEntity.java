7e3ee4a0116d83bc97abce05e66e55ba3fd2b7fb
lisettealvarez <1412452@utp.edu.pe>
2017-11-23 19:34:38 -0500

    correccion de locationentity

--- src/pe/com/ctaf/beautyapp/models/LocationsEntity.java
+++ src/pe/com/ctaf/beautyapp/models/LocationsEntity.java
@@ -5,87 +5,42 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class LocationsEntity extends BaseEntity {
+public class LocationsEntity extends BaseEntity{
 
-    public LocationsEntity(Connection connection, String tableName) {
-        super(connection, tableName);
+
+    public LocationsEntity(Connection connection) {
+        super(connection,"location");
     }
 
-    public LocationsEntity() {
-        super();
-        setTableName("location");
-    }
-    public Location findById(String id) {
-        return findByCriteria(
-                String.format("WHERE id = '%d'", id)).get(0);
-    }
+
+    public LocationsEntity() { super(); }
 
     public List<Location> findByCriteria(String criteria) {
+        String sql = getDefaultQuery() + (criteria.equalsIgnoreCase("") ? "" : " WHERE " + criteria);
+        List<Location> locations = new ArrayList<>();
         try {
-            ResultSet rs = getConnection()
+            ResultSet resultSet = getConnection()
                     .createStatement()
-                    .executeQuery(
-                            getBaseStatement()
-                                    .concat(criteria));
-            List<Location> locations = new ArrayList<>();
-            while(rs.next())
-                locations.add(Location.build(rs));
-
+                    .executeQuery(sql);
+            if (resultSet == null) return null;
+            while (resultSet.next()) {
+                locations.add(Location.build(resultSet));
+            }
             return locations;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return null;
-
-    }
-    public Location findByDespartament(String departament) {
-        return findByCriteria(
-                String.format("WHERE departament = '%s'", departament)).get(0);
     }
 
-    public Location findByProvince(String province){
-        return  findByCriteria(String.format("WHERE province= '%s'", province)).get(0);
-    }
-    public Location findByDistrict(String district){
-        return  findByCriteria(String.format("WHERE district= '%s'", district)).get(0);
-    }
-    public Location findByAddress(String address){
-        return  findByCriteria(String.format("WHERE address= '%s'", address)).get(0);
-    }
-    public List<Location> findAll() {
+    List<Location>findAll(){
         return findByCriteria("");
     }
 
-    public List<Location> findAllWithLocations() {
-        return findByCriteria("id IN (SELECT DISTINCT id FROM location)");
+    public Location findById(String id){
+        String criteria = " id = '" + id + "'";
+        return findByCriteria(criteria).get(0);
     }
 
-    public boolean create(Location location) {
-        return executeUpdate(String.format(
-                "INSERT INTO %s(id, departament,province,district,address) VALUES( '%d', '%s', '%a', '%b', '%c')",
-                getTableName(),location.getId(),location.getDepartament(),location.getProvince(),location.getDistrict(),location.getAddress()));
-    }
-    public boolean create(String id, String departament,String province, String district,String address) {
-        return create(new Location(id,departament,province,district,address));
-    }
 
-    public boolean update(String id, String departament,String province, String district,String address) {
-        return executeUpdate(String.format(
-                "UPDATE %s SET departament = '%s', province = '%d', district ='%a', address= '%b' WHERE id = '%s'",
-                getTableName(), departament,province,district,address,id));
-    }
-
-    public boolean update(Location location) {
-        return update(location.getId(),location.getDepartament(),location.getProvince(),location.getDistrict(),location.getAddress());
-    }
-
-    public boolean erase(String id) {
-        return executeUpdate(String.format("DELETE FROM %s WHERE id= '%s'",
-                getTableName(), id));
-    }
-
-    public boolean erase(Location location) {
-        return executeUpdate(String.format("DELETE FROM %s WHERE id= '%s'",
-                getTableName(), location.getId()));
-    }
 }
